bd4c80769ca35987299df300bdca9c21125cecd3
MirekMajka <miroslav.majka@gmail.com>
2019-02-02 14:42:22 +0100

    pierwszy

---
+++ src/pl/majka/Car.java
@@ -0,0 +1,13 @@
+package pl.majka;
+
+public class Car {
+    public String numerRejestracyjny;
+    public String Cena;
+    public String Wlasciciel;
+
+    public Car(String numerRejestracyjny, String Cena, String Wlasciciel){
+        this.numerRejestracyjny = numerRejestracyjny;
+        this.Cena = Cena;
+        this.Wlasciciel = Wlasciciel;
+    }
+}
